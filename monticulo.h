6982419fae73c27ec8942140baef59f3f2097896
lawMS <50967742+lawMS@users.noreply.github.com>
2020-07-26 11:40:59 -0500

    Update monticulo.h
    Funcion Atender

--- monticulo.h
+++ monticulo.h
@@ -12,18 +12,54 @@ class Monticulo{
 	}
 	
 	void insertar(int nodo);
-	int atender();
+	int atender(int *Arr, int tam,int posAct);
 	void imprimir();
 	bool monticulo_lleno();
 	
 };
-
+//
+int subatender(int *Arr, int posAct,int ultimo,int tam){
+	 if(*(Arr+posAct*2)>*(Arr+posAct*2+1) && *(Arr+posAct*2)>ultimo){
+	 	
+            *(Arr+posAct)=*(Arr+posAct*2);
+	 	    *(Arr+posAct*2)=posAct;
+	 	    
+	 }else if(*(Arr+posAct*2+1)>*(Arr+posAct*2) && *(Arr+posAct*2+1)>ultimo){
+	 	
+	 	    *(Arr+posAct)=*(Arr+posAct*2+1);
+	 	    *(Arr+posAct*2+1)=posAct;
+	 	    
+	 }else if(ultimo>*(Arr+posAct*2) && ultimo>*(Arr+posAct*2+1)){
+	 	
+	 	    *(Arr+posAct)=ultimo;
+	 }
+tam =-- tam;
+	}
 void Monticulo::insertar(int nodo){	
 //Hay que hacer estas funciones
 }
-
-int Monticulo::atender(){	
-//Hay que hacer estas funciones
+int Monticulo::atender(int *Arr, int tam, int posAct){
+	int ultimo= *(Arr+tam);
+ 	
+	if(*(Arr+2)>ultimo &&*(Arr+2)>*(Arr+3)){
+		*(Arr)=*(Arr+2);
+		posAct=*(Arr+2);
+	while(posAct=!NULL){
+             int subatender(int *Arr, int posAct,int ultimo,int tam);
+	}
+	}else if(ultimo>*(Arr+2) && ultimo >*(Arr+3)){
+		
+		*(Arr)=ultimo;
+		
+	}else if(*(Arr+3)>ultimo &&*(Arr+3)>*(Arr+2)){
+		
+		*(Arr)=*(Arr+3);
+		posAct=*(Arr+3);
+		
+		while(posAct=!NULL){
+			 int subatender(int *Arr, int posAct,int ultimo,int tam);
+	}
+}
 }
 
 void Monticulo::imprimir(){	
