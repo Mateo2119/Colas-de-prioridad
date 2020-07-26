a26a23b2ec8c8a602d1567e4ffedc2cdc43b5290
Mateo <narinomateo2119@gmail.com>
2020-07-26 14:35:14 -0500

    insertar y atender

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "monticulo.h"
 
@@ -22,28 +21,4 @@ int main(int argc, char *argv[]){
 	
 
 
-=======
-#include <iostream>
-#include "monticulo.h"
 
-using namespace std;
-
-int main(int argc, char *argv[]){
-	
-	Monticulo lista(7);
-	//Insertar
-	lista.insertar(100);
-	lista.insertar(90);
-	lista.insertar(80);
-	lista.insertar(110);
-	lista.insertar(95);
-	lista.insertar(120);
-	lista.insertar(130);
-	lista.imprimir();
-}
-	
-	
-	
-
-
->>>>>>> b6e316a52852159b7f343b8fb9c6621911f198b5
