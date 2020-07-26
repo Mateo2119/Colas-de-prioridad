62f9bc199507103f8d242934681587abfd50fa0a
Mateo <narinomateo2119@gmail.com>
2020-07-26 13:18:10 -0500

    Insertar funcionando

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
+	
+	Monticulo lista(7);
+	//Insertar
+	lista.insertar(100);
+	lista.insertar(90);
+	lista.insertar(80);
+	lista.insertar(110);
+	lista.insertar(95);
+	lista.insertar(120);
+	lista.insertar(130);
+	lista.imprimir();
 }
+	
+	
+	
 
 
--- monticulo.h
+++ monticulo.h
@@ -3,31 +3,57 @@
 #include <iostream>
 using namespace std;
 
+struct Nodo{
+	int prioridad;	
+};
+
 
 class Monticulo{
 	int *Arr;
-	int tam, posAct;
+	int tam, posAct = 1;
+	int lista[];
 	
-	public: Monticulo(int nodo){
+	public: Monticulo(int tamano){
+		tam = tamano;
+		int lista[tam];		
 	}
 	
-	void insertar(int nodo);
+	void insertar(int prioridad);
 	int atender();
 	void imprimir();
-	bool monticulo_lleno();
-	
+	bool monticulo_lleno();	
 };
 
-void Monticulo::insertar(int nodo){	
-//Hay que hacer estas funciones
+void Monticulo::insertar(int prioridad){
+	int aux;
+	if(posAct == 1){
+		lista[posAct] = prioridad;
+		posAct = posAct + 1;
+	}
+	else if(prioridad < lista[(posAct/2)]){
+		lista[posAct] = prioridad;
+		posAct = posAct + 1;
+	}
+	else{ //Si prioridad es mayor a posAct
+		aux = posAct; //auxiliar con la posAct otiginal
+		while(prioridad > lista[(aux/2)]){ //Mientras que prioridad sea mayor a posAct			
+			lista[aux] = lista[(aux/2)]; //Poner en posAct al padre
+			lista[(aux/2)] = prioridad; //ponemos en la posicion padre a la prioridad
+			aux = aux/2;
+		}
+		posAct = posAct + 1;			
+	}	
 }
 
-int Monticulo::atender(){	
+int Monticulo::atender(){
 //Hay que hacer estas funciones
 }
 
 void Monticulo::imprimir(){	
-//Hay que hacer estas funciones
+	cout<<"tamaño: "<<tam<<endl;
+	for(int i=1; i<=tam;i++){
+		cout<<i<<" valor: "<<lista[i]<<endl;
+	}
 }
 
 bool Monticulo::monticulo_lleno(){	
